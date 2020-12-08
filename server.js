146839edd78e22446d808b8152ca50949a4b27c2
Lays Barbosa <barbosalays36@gmail.com>
2020-12-08 10:15:54 -0300

    [

---
+++ server.js
@@ -0,0 +1,9 @@
+//Importar as dependencias
+const express = require('express')
+const bodyParser = require('body-parser')
+
+//Iniciando o express
+const app = express()
+
+//escutando a porta 3000
+app.listen(3000)
